b425bb2352e0cfaab55aadcfa52cecea204687e3
irhabano234 <irha.bano@gmail.com>
2018-06-21 17:32:35 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,34 @@
+def gangham_style
 
-# Code your methods below
+def starting_stance
+puts "Step 1 Plant legs far apart, bend knees slightly and keep posture loose."
+end
+
+def base_footwork
+puts "Step 2 Lift right foot."
+puts "Step 3 Return right foot to the ground."
+puts "Step 4 Finishing with a small skip-step backward."
+end
+
+def skip_step{starting_foot}
+puts "Step 5 Lower left foot to the ground."
+puts "Step 6 Bounce left foot back up slightly, kicking it a few inches back."
+puts "Step 7 Left, Right, Left and Left."
+puts "Step 8 Lower right foot to the ground."
+puts "Step 9 Bounce right foot back up slightly, kicking it a few inches back."
+puts "Step 10 Right, Left, Right and Right."
+end
+
+def bob_the_reins
+puts "Step 11 Holding your arms out in front of you, straight and at chest level."
+puts "Step 12 Cross your right wrists over your left and hold them together"
+puts "Step 13 Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end
+
+def lasso
+puts "Step 14 Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+puts "Step 15 Lift right arm up and point right elbow diagonally to the right."
+puts "Step 16 Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+
+end
